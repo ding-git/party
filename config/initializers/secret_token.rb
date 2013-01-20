804996a879763a991fa838113f632bb98e6261d4
dingzhang <dingzhyush@gmail.com>
2013-01-20 20:38:55 +0800

    p1
    pa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Party::Application.config.secret_token = '4aa47bd0aa95d56e2bf6aef5ea57eb0361dfc930a81016fa68623fe11e0af3296d95db188b4d0b1a11daf20a222ab02a44bcd3c7ec55c3f3c605c979e928234a'
